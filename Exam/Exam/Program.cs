68b193accd99af133652ef4dc3b3c653f9a3ca78
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-06 12:03:32 +0200

    Created Project

---
+++ Exam/Exam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exam
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
